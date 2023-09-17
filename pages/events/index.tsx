b558378305a1f80f73ce995c56d9f13d46ac45d0
Sam Dunegan <cesarcisneros9@gmail.com>
2023-09-17 16:53:38 -0500

    update logo

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,17 +1,19 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 import LottieControl from "../../components/lottie/lottie";
 import construction from "../../lib/lottie/construction.json";
 
 type Props = {};
 
 const Events = (props: Props) => {
+  const [firstClick, setFirstClick] = useState(false);
   return (
     <div
       style={{
         display: "flex",
         alignItems: "center",
         flexDirection: "column",
+        width: "100%",
       }}
     >
       <Head>
@@ -22,9 +24,25 @@ const Events = (props: Props) => {
         />
         <meta
           name="keywords"
-          content="food, san marcos, tx, texas, kolache, taco, burger, breakfast, lunch, dinner, restaurant"
+          content="catering, food, san marcos, tx, texas, kolache, taco, burger, breakfast, lunch, dinner, restaurant"
         />
       </Head>
+      {/* <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: firstClick ? "1fr 1fr 1fr" : "1fr 1fr",
+          backgroundColor: "red",
+          width: "100%",
+          maxWidth: "500px",
+          gridColumnGap: "20px",
+          transition: "2s ease-in-out",
+        }}
+        onClick={() => setFirstClick(true)}
+      >
+        <div style={{}}>hello</div>
+        <div style={{}}>hello</div>
+        <div style={{}}>hello</div>
+      </div> */}
       <h1 style={{ textAlign: "center" }}>Currently Under Construction</h1>
       <div
         style={{
@@ -40,3 +58,15 @@ const Events = (props: Props) => {
 };
 
 export default Events;
+
+const style = {
+  ["button-container"]: {
+    display: "grid",
+    gridTemplateColumns: "repeat(2, 1fr)",
+    backgroundColor: "red",
+    width: "100%",
+    maxWidth: "500px",
+    gridColumnGap: "20px",
+  },
+  ["button"]: {},
+};
