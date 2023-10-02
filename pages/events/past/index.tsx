7ae94fe2c388160a4d22045a904f2488dddaa7ae
Sam Dunegan <cesarcisneros9@gmail.com>
2023-10-02 14:39:49 -0500

    updated past events, testing it out

--- pages/events/past/index.tsx
+++ pages/events/past/index.tsx
@@ -8,6 +8,7 @@ import BlockContent from "@sanity/block-content-to-react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import classes from "./past.module.css";
+import { AddToCalendarButton } from "add-to-calendar-button-react";
 
 import { AllEventsType, EventDetails } from "../../../lib/types";
 import useWindowDimensions from "../../../hooks/useWindowDimensions";
@@ -29,114 +30,128 @@ const PastEvents = ({ data }: Props) => {
       style={{
         display: "grid",
         gridTemplateColumns:
-          winDim.width && winDim.width <= 650
+          winDim.width && winDim.width <= 800
             ? "1fr"
             : winDim.width && winDim.width <= 1200
             ? "1fr 1fr"
             : "1fr 1fr 1fr",
         rowGap: 24,
         columnGap: 24,
-        margin: 10,
+        margin: "10px 20px",
         gridAutoRows: "minmax(200px, auto)",
         maxWidth: 1800,
       }}
     >
       {data.allEvents.map((event: EventDetails) => (
         <div
-          style={{
-            display: "flex",
-            width: "100%",
-            // height: "100%",
-            border: "#f1f1f1 solid 3px",
-            borderRadius: 12,
-            overflow: "hidden",
-            zIndex: 1000,
-          }}
+          style={{ display: "flex", flexDirection: "column", height: "100%" }}
         >
           <div
             style={{
-              // flex: 1.5,
-              minWidth: 150,
-              maxWidth: 200,
-              width: "50%",
-              alignSelf: "center",
-              cursor: "zoom-in",
-              // height: "100%",
-              overflow: "hidden",
-            }}
-          >
-            <Carousel
-              showThumbs={false}
-              axis="vertical"
-              onClickItem={() => {
-                setCarousel(event);
-              }}
-              dynamicHeight={false}
-            >
-              {event.flyer?.map((flyer) => (
-                <div
-                  style={{
-                    height: "100%",
-                    // position: "relative",
-                    // objectFit: "cover",
-                    overflow: "hidden",
-                  }}
-                >
-                  <img style={{}} src={flyer.asset.url} />
-                </div>
-              ))}
-            </Carousel>
-          </div>
-          <div
-            style={{
-              // flex: 2,
               display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              textAlign: "center",
+              // height: 300,
+              height: "100%",
+              width: "100%",
+              border: "#f1f1f1 solid 3px",
+              overflow: "hidden",
+              zIndex: 1000,
+              borderRadius: 12,
             }}
           >
-            <h3 style={{ margin: "10px" }}>
-              <span>{event.name}</span>
-            </h3>
-            <h4
+            <div
+              style={{
+                minWidth: 100,
+                maxWidth: 250,
+                width: "100%",
+                alignSelf: "center",
+                cursor: "zoom-in",
+                overflow: "hidden",
+              }}
+            >
+              <Carousel
+                showThumbs={false}
+                axis="vertical"
+                onClickItem={() => {
+                  setCarousel(event);
+                }}
+                dynamicHeight={false}
+              >
+                {event.flyer?.map((flyer) => (
+                  <div
+                    style={{
+                      height: "100%",
+                      overflow: "hidden",
+                    }}
+                  >
+                    <img style={{}} src={flyer.asset.url} />
+                  </div>
+                ))}
+              </Carousel>
+            </div>
+            <div
               style={{
                 display: "flex",
                 flexDirection: "column",
-                margin: "0 0 12px 0",
-                fontFamily: "Fauna One, Serif",
-                fontWeight: "lighter",
-                fontSize: 14,
-                backgroundColor: "#f1f1f1",
-                color: "#1b1b1b",
-                padding: 5,
-                borderRadius: 10,
+                alignItems: "center",
+                textAlign: "center",
+                width: "100%",
               }}
             >
-              <span
+              <h3 style={{ margin: "10px" }}>
+                <span>{event.name}</span>
+              </h3>
+              <h4
                 style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  margin: "0 0 12px 0",
                   fontFamily: "Fauna One, Serif",
                   fontWeight: "lighter",
+                  fontSize: 14,
+                  backgroundColor: "#f1f1f1",
+                  color: "#1b1b1b",
+                  padding: 5,
+                  borderRadius: 10,
                 }}
               >
-                {new Date(event.event_date).toLocaleDateString("en-us", {
-                  year: "numeric",
-                  month: "short",
-                  day: "numeric",
-                })}
-              </span>
-              <span style={{ fontFamily: "Fauna One, Serif" }}>
-                @{" "}
-                {new Date(event.event_date).toLocaleTimeString("en-US", {
-                  timeZone: "America/Chicago",
-                  hour: "numeric",
-                  minute: "numeric",
-                })}
-              </span>
-            </h4>
-            <div className={classes.block_content}>
-              <BlockContent blocks={event.event_descriptionRaw} />
+                <span
+                  style={{
+                    fontFamily: "Fauna One, Serif",
+                    fontWeight: "lighter",
+                  }}
+                >
+                  {new Date(event.event_date).toLocaleDateString("en-us", {
+                    year: "numeric",
+                    month: "short",
+                    day: "numeric",
+                  })}
+                </span>
+                <span style={{ fontFamily: "Fauna One, Serif" }}>
+                  @{" "}
+                  {new Date(event.event_date).toLocaleTimeString("en-US", {
+                    timeZone: "America/Chicago",
+                    hour: "numeric",
+                    minute: "numeric",
+                  })}
+                </span>
+              </h4>
+              <div className={classes.block_content}>
+                <BlockContent blocks={event.event_descriptionRaw} />
+              </div>
             </div>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <AddToCalendarButton
+              name="Test-Event"
+              startDate="2023-05-22"
+              options={["Apple", "Google", "Yahoo", "iCal"]}
+              listStyle="overlay"
+              trigger="click"
+              styleLight="--btn-background: #f1f1f1; --btn-text: #1b1b1b; --font: Fauna One, Serif"
+
+              // buttonStyle="custom"
+              // customCss="../../styles/globals.css"
+            ></AddToCalendarButton>
           </div>
         </div>
       ))}
