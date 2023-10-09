72a23be6c0800ff7f7d86722e149cac85590928d
Sam Dunegan <cesarcisneros9@gmail.com>
2023-10-09 01:17:59 -0500

    updates

--- pages/events/past/index.tsx
+++ pages/events/past/index.tsx
@@ -3,16 +3,14 @@ import EventsPageLayout from "../../../components/layout/eventsPageLayout";
 import { gql } from "@apollo/client";
 import { GetStaticProps } from "next";
 import client from "../../../lib/apollo";
-import Image from "next/image";
-import BlockContent from "@sanity/block-content-to-react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import classes from "./past.module.css";
-import { AddToCalendarButton } from "add-to-calendar-button-react";
 import { useRouter } from "next/router";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { AllEventsType, EventDetails } from "../../../lib/types";
 import useWindowDimensions from "../../../hooks/useWindowDimensions";
+import EventCard from "../../../event-card/event-card";
 
 type Props = {
   data: AllEventsType;
@@ -38,64 +36,9 @@ const PastEvents = ({ data }: Props) => {
   }, []);
 
   return (
-    <div className={classes.past_events_grid_container} style={{}}>
+    <div className={classes.past_events_grid_container}>
       {data.allEvents.map((event: EventDetails) => (
-        <div className={classes.past_events_container}>
-          <div className={classes.past_events_card}>
-            <Carousel
-              className={classes.crsl}
-              showThumbs={false}
-              axis="vertical"
-              onClickItem={() => {
-                setCarousel(event);
-              }}
-              autoPlay
-              infiniteLoop
-              interval={6000}
-            >
-              {event.flyer?.map((flyer) => (
-                <img src={flyer.asset.url} />
-              ))}
-            </Carousel>
-            <div className={classes.past_event_content}>
-              <h3>
-                <span>{event.name}</span>
-              </h3>
-              <h4>
-                <span>
-                  {new Date(event.event_date).toLocaleDateString("en-us", {
-                    year: "numeric",
-                    month: "short",
-                    day: "numeric",
-                  })}
-                </span>
-                {event.show_time && (
-                  <span>
-                    @{" "}
-                    {new Date(event.event_date).toLocaleTimeString("en-US", {
-                      timeZone: "America/Chicago",
-                      hour: "numeric",
-                      minute: "numeric",
-                    })}
-                  </span>
-                )}
-              </h4>
-              <div className={classes.block_content}>
-                <BlockContent blocks={event.event_descriptionRaw} />
-              </div>
-            </div>
-          </div>
-          <div className={classes.button_container}>
-            <AddToCalendarButton
-              name={event.name}
-              startDate={new Date(event.event_date).toISOString().split("T")[0]}
-              options={["Apple", "Google", "Yahoo", "iCal"]}
-              listStyle="overlay"
-              trigger="click"
-              styleLight="--btn-background: #f1f1f1; --btn-text: #1b1b1b; --font: Fauna One, Serif"
-            ></AddToCalendarButton>
-          </div>
-        </div>
+        <EventCard event={event} setCarousel={setCarousel} />
       ))}
       {carousel.name ? (
         <div
@@ -120,8 +63,8 @@ const PastEvents = ({ data }: Props) => {
           >
             <Carousel
               showThumbs={winDim.height && winDim.height < 600 ? false : true}
-              axis="vertical"
               autoPlay
+              dynamicHeight
               infiniteLoop
               interval={6000}
               className={`${classes.modal_crsl} modal_crsl`}
--- pages/events/upcoming/index.tsx
+++ pages/events/upcoming/index.tsx
@@ -3,24 +3,39 @@ import EventsPageLayout from "../../../components/layout/eventsPageLayout";
 import LottieControl from "../../../components/lottie/lottie";
 import noEvent from "../../../lib/lottie/no_event_calender.json";
 import Link from "next/link";
-type Props = {};
+import { EventDetails } from "../../../lib/types";
+type Props = {
+  event: EventDetails;
+};
 
-const UpcomingEvents = (props: Props) => {
+const UpcomingEvents = ({ event }: Props) => {
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        width: "100%",
-        marginTop: 30,
+        maxWidth: "550px",
+        marginTop: 0,
+        marginLeft: 30,
+        marginRight: 30,
       }}
     >
-      <h3 style={{ textAlign: "center" }}>No events planned at this time. </h3>
-      <p>
+      <h3 style={{ textAlign: "center", fontSize: 22 }}>
+        No events planned at this time.{" "}
+      </h3>
+      <div
+        style={{
+          maxWidth: 200,
+          margin: "10px 20px 20px 20px",
+        }}
+      >
+        <LottieControl lottie={noEvent} speed={0.5} />
+      </div>
+      <p style={{ fontSize: 16, textAlign: "justify", lineHeight: 1.5 }}>
         Please check back later or visit our{" "}
         <Link href="/events" style={{ color: "#0DC0C6" }}>
-          Events Page
+          Events Page{" "}
         </Link>
         to look at our{" "}
         <Link href="/events/past" style={{ color: "#0DC0C6" }}>
@@ -32,16 +47,6 @@ const UpcomingEvents = (props: Props) => {
         </Link>{" "}
         with us.
       </p>
-      <div
-        style={{
-          width: "80%",
-          maxWidth: 300,
-          maxHeight: 600,
-          padding: "60px 20px",
-        }}
-      >
-        <LottieControl lottie={noEvent} speed={0.5} />
-      </div>
     </div>
   );
 };
